7525b20839115832a40b3e9d8a1d4e3bee8035d0
asvNathalia <94073807+asvNathalia@users.noreply.github.com>
2023-10-27 11:44:35 -0300

    Delete Projeto_Integrador directory

--- Projeto_Integrador/static/script_time.js
+++
@@ -1,9 +0,0 @@
-const mainElement = document.querySelector('.conteudo-principal');
-
-const horaAtual = new Date().getHours();
-
-if (horaAtual >= 6 && horaAtual < 18) {
-    mainElement.classList.add('main-dia');
-} else {
-    mainElement.classList.add('main-noite');
-}
